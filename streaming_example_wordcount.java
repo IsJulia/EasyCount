2340e78fa6f9fbfc3396bae43d00658b555fc78a
Julia <78349306+IsJulia@users.noreply.github.com>
2022-08-28 09:56:07 +0800

    Update streaming_example_wordcount.java
    a complete, working example of streaming window word count application, that counts the words coming from a web socket in 5 second windows.

--- streaming_example_wordcount.java
+++ streaming_example_wordcount.java
@@ -1,11 +1,35 @@
-case class WordWithCount(word: String, count: Long)
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
 
-val text = env.socketTextStream(host, port, '\n')
+public class WindowWordCount {
 
-val windowCounts = text.flatMap { w => w.split("\\s") }
-  .map { w => WordWithCount(w, 1) }
-  .keyBy("word")
-  .window(TumblingProcessingTimeWindow.of(Time.seconds(5)))
-  .sum("count")
+    public static void main(String[] args) throws Exception {
 
-windowCounts.print()
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("localhost", 9999)
+                .flatMap(new Splitter())
+                .keyBy(value -> value.f0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .sum(1);
+
+        dataStream.print();
+
+        env.execute("Window WordCount");
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word: sentence.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+
+}
