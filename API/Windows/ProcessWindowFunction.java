d50a5408459d38650f2add5bd5101d938ea570fb
Julia <78349306+IsJulia@users.noreply.github.com>
2022-08-28 22:45:08 +0800

    Create ProcessWindowFunction.java
    incremental Window Aggregation with ReduceFunction.

---
+++ API/Windows/ProcessWindowFunction.java
@@ -0,0 +1,27 @@
+DataStream<SensorReading> input = ...;
+
+input
+  .keyBy(<key selector>)
+  .window(<window assigner>)
+  .reduce(new MyReduceFunction(), new MyProcessWindowFunction());
+
+// Function definitions
+
+private static class MyReduceFunction implements ReduceFunction<SensorReading> {
+
+  public SensorReading reduce(SensorReading r1, SensorReading r2) {
+      return r1.value() > r2.value() ? r2 : r1;
+  }
+}
+
+private static class MyProcessWindowFunction
+    extends ProcessWindowFunction<SensorReading, Tuple2<Long, SensorReading>, String, TimeWindow> {
+
+  public void process(String key,
+                    Context context,
+                    Iterable<SensorReading> minReadings,
+                    Collector<Tuple2<Long, SensorReading>> out) {
+      SensorReading min = minReadings.iterator().next();
+      out.collect(new Tuple2<Long, SensorReading>(context.window().getStart(), min));
+  }
+}
